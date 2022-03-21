aca4db3d625af29c812a78354a1fd261702d7dcc
迎松 <yanyingsong@outlook.com>
2022-03-21 18:20:47 +0800

    Update notsleep.bat

--- notsleep.bat
+++ notsleep.bat
@@ -1,8 +1,8 @@
 @echo off
-echo Set wshShell = WScript.CreateObject("WScript.Shell") > NUMLOCK.vbs
-echo wshShell.SendKeys "{NUMLOCK}" >> NUMLOCK.vbs
-echo WScript.sleep 60000 >> NUMLOCK.vbs
+echo Set wshShell = WScript.CreateObject("WScript.Shell") > numlock.vbs
+echo wshShell.SendKeys "{NUMLOCK}" >> numlock.vbs
+echo WScript.sleep 60000 >> numlock.vbs
 echo Keeping this window prevents the computer from going to sleep!
 :start
-NUMLOCK.vbs
+numlock.vbs
 goto start
